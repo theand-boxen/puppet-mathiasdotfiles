ce3dd61baf4359b7d936e900c6927e21d5983e34
Heesang Chae <theand@gmail.com>
2014-12-21 17:15:32 +0900

    change dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class mathiasdotfiles {
   exec {'install-mathias-dotfiles':
     command => 'cd ~ && git clone https://github.com/theand/mathiasdotfiles.git',
-    creates => "/Users/${::luser}/dotfiles",
+    creates => "/Users/${::luser}/mathiasdotfiles",
     timeout => 0
   }
 }
